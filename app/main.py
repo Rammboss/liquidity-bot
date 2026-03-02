83382fb3166dbc7e2296c28cc4c3fe805e57942b
Thomas <thomas.lippert93@gmail.com>
2026-03-02 20:02:50 +0100

    add new main flags

--- app/main.py
+++ app/main.py
@@ -23,30 +23,48 @@ class Application:
       starting_balance_eth: float,
       starting_balance_eurc: float,
       starting_balance_usdc: float,
-      target_qty: float
+      target_qty: float,
+      arbitrage_bot_enabled: bool,
+      uniswap_position_manger_enabled: bool,
+      indexer_enabled: bool,
   ):
     self.db.init()
 
-    indexer = IndexerService(self.db)
-    uniswap_position_analyzer = UniswapPositionAnalyzer(self.db)
-    executor = Executor()
-    coinbase_uniswap_arbitrage_analyzer = UniswapArbitrageAnalyzer(
-      starting_date,
-      starting_balance_eth,
-      starting_balance_eurc,
-      starting_balance_usdc,
-      target_qty,
-      COINBASE_EURC_USDC_TICKER,
-      EURO_USDC_UNI_V3_POOL_ADDRESS,
-      Tokens.EURC,
-      Tokens.USDC,
-      executor
-    )
+    tasks = []
 
-    await asyncio.gather(
-      coinbase_uniswap_arbitrage_analyzer.run(),
-      executor.run()
-    )
+    # 1. Indexer Service
+    if indexer_enabled:
+      indexer = IndexerService(self.db)
+      tasks.append(indexer.run())
+
+    # 2. Uniswap Position Analyzer
+    if uniswap_position_manger_enabled:
+      uniswap_position_analyzer = UniswapPositionAnalyzer(self.db)
+      tasks.append(uniswap_position_analyzer.run())
+
+    # 3. Arbitrage & Executor
+    if arbitrage_bot_enabled:
+      executor = Executor()
+      tasks.append(executor.run())
+
+      coinbase_uniswap_arbitrage_analyzer = UniswapArbitrageAnalyzer(
+        starting_date,
+        starting_balance_eth,
+        starting_balance_eurc,
+        starting_balance_usdc,
+        target_qty,
+        COINBASE_EURC_USDC_TICKER,
+        EURO_USDC_UNI_V3_POOL_ADDRESS,
+        Tokens.EURC,
+        Tokens.USDC,
+        executor
+      )
+      tasks.append(coinbase_uniswap_arbitrage_analyzer.run())
+
+    if tasks:
+      await asyncio.gather(*tasks)
+    else:
+      print("No services enabled. Standing by.")
 
   def run(
       self,
@@ -54,14 +72,20 @@ class Application:
       starting_balance_eth: float,
       starting_balance_eurc: float,
       starting_balance_usdc: float,
-      target_qty: float
+      target_qty: float,
+      arbitrage_bot_enabled: bool,
+      uniswap_position_manger_enabled: bool,
+      indexer_enabled: bool,
   ):
     asyncio.run(self._start_services(
       starting_date=starting_date,
       starting_balance_eth=starting_balance_eth,
       starting_balance_eurc=starting_balance_eurc,
       starting_balance_usdc=starting_balance_usdc,
-      target_qty=target_qty
+      target_qty=target_qty,
+      arbitrage_bot_enabled=arbitrage_bot_enabled,
+      uniswap_position_manger_enabled=uniswap_position_manger_enabled,
+      indexer_enabled=indexer_enabled
     ))
 
 
@@ -72,5 +96,8 @@ if __name__ == "__main__":
     starting_balance_eth=0.36,
     starting_balance_eurc=2803.649488,
     starting_balance_usdc=2997.099793,
-    target_qty=4000
+    target_qty=4000,
+    arbitrage_bot_enabled=True,
+    uniswap_position_manger_enabled=False,
+    indexer_enabled=False
   )
